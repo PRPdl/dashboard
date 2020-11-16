a4e76fb5b63e5238fbd4ded324d8579d3762104e
prpdl <prp_slayer@hotmail.com>
2020-11-16 13:37:39 +1030

    v-navigation added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,44 @@
 <template>
   <nav>
-    <v-toolbar
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-        <v-toolbar-title class="text-uppercase red--text">
-          <span class="font-weight-bold">TOdo:</span>
-          <span>Pradip</span>
-        </v-toolbar-title>
-        <v-spacer />
-        <v-btn v-if="this.loggedin" text color="yellow">
-          <span>Sign Out</span>
-          <v-icon right>exit_to_app</v-icon>
-        </v-btn>
+    <v-app-bar
+      flat
+      app
+      absolute
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon
+        color="red"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase red--text">
+        <span class="font-weight-bold">TOdo:</span>
+        <span>Pradip</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn v-if="this.loggedin" text color="yellow">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
 
-        <v-btn v-if="!this.loggedin" text color="red">
-          <span>Sign In</span>
-          <v-icon right>login</v-icon>
-        </v-btn>
-    </v-toolbar>
-
-    <v-naviagation-drawer v-model="drawer" app class="grey">
-        <p>Test</p>
-    </v-naviagation-drawer>
-
+      <v-btn v-if="!this.loggedin" text color="red">
+        <span>Sign In</span>
+        <v-icon right>login</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app absolute  class="blue">
+      <p>Test</p>
+    </v-navigation-drawer>
   </nav>
-
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            loggedin: false,
-            drawer: true,
-
-        }
-    }
+  data() {
+    return {
+      loggedin: false,
+      drawer: false,
+    };
+  },
 };
 </script>
 
