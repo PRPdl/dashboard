5e8c288ba8970b5baf8fd827fdf07cd7d949e6bb
prpdl <prp_slayer@hotmail.com>
2020-11-26 15:36:25 +1030

    S3 test

--- src/views/resetPass.vue
+++ src/views/resetPass.vue
@@ -1,42 +1,7 @@
 <template>
   <v-layout align-center justify-center>
     <v-flex xs12 sm12 md6 lg4>
-      <v-card class="text-center" max-width="">
-        <v-card-title primary-title class="justify-center">
-          Password Reset
-        </v-card-title>
-        <v-card-subtitle>
-          or Just <a href="/login" class="text-decoration-none">Login</a>
-        </v-card-subtitle>
-        <v-form ref="loginForm" v-model="isValid">
-          <v-card-subtitle v-show="emailError" ref="error" class="red--text pa-0">{{
-            emailError
-          }}</v-card-subtitle>
-
-          <v-text-field
-            class="px-16"
-            name="email"
-            ref="emailField"
-            v-model="email"
-            :rules="emailRules"
-            label="Enter your Email"
-            hint="Enter a registered email"
-            prepend-icon="mdi-email"
-          ></v-text-field>
-
-          <v-btn
-            color="primary"
-            :disabled="!isValid"
-            @click="reset"
-            :loading="resetLoading"
-            >Reset</v-btn
-          >
-        </v-form>
-        <v-card-subtitle>
-          Need new Account?
-          <a href="/register" class="text-decoration-none">Register</a>
-        </v-card-subtitle>
-      </v-card>
+     
     </v-flex>
   </v-layout>
 </template>
